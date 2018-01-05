54aaf71e02c01622275150d0b4b90eff6925636d
chen1zee <beno1992>
2018-01-05 18:17:22 +0800

    [update]something

--- gulp-other-util/gulp-import-path-change.js
+++ gulp-other-util/gulp-import-path-change.js
@@ -2,6 +2,9 @@ const through = require('through2');
 
 function gulpImportPathChange() {
     return through.obj(function(file, enc, cb) {
+        const pathBaseOnSrc = file.path.replace(file.base, '');
+        // todo continue
+        console.log(pathBaseOnSrc);
         this.push(file);
         cb();
     })
