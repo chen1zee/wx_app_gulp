dde65ab931f398f00a83761b0e00cc8ec17d5e04
chen1zee <beno1992>
2018-01-05 17:13:45 +0800

    [update]去掉一些没有的console

--- gulp-other-util/gulp-import-path-change.js
+++ gulp-other-util/gulp-import-path-change.js
@@ -2,12 +2,6 @@ const through = require('through2');
 
 function gulpImportPathChange() {
     return through.obj(function(file, enc, cb) {
-        for (let i in file) {
-            console.log('**********');
-            console.log(i);
-            console.log(file[i]);
-            console.log('**********');
-        }
         this.push(file);
         cb();
     })
